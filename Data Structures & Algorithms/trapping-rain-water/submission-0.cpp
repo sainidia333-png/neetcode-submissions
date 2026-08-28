e05a3195de65ae78a10d62bf700ec69b88610f24
sainidia333-png <saini.dia333@gmail.com>
2026-08-28 21:38:44 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l=0;
+        int r=height.size()-1;
+        int leftmax=0,rightmax=0,water=0;
+        while(l<r){
+        if(height[l]<height[r]){
+            if(height[l]>=leftmax){
+                leftmax=height[l];
+                l++;
+            }else{
+                water+=leftmax-height[l];
+                l++;
+            }
+        }else{
+            if(height[r]>=rightmax){
+                rightmax=height[r];
+                r--;
+            }else{
+                water+=rightmax-height[r];
+                r--;
+            }
+        }
+    }
+    return water;
+    }
+};
