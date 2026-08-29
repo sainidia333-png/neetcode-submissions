da3a84436d5e57ec9900e8f106cea56d69809603
sainidia333-png <saini.dia333@gmail.com>
2026-08-30 01:28:21 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>mpp;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        
+        for(auto it:mpp){
+            pq.push({it.second,it.first});
+            if(pq.size()>k){
+                pq.pop();
+            }
+            
+    
+        }
+                while(!pq.empty()) {
+    ans.push_back(pq.top().second);
+    pq.pop();
+}
+        return ans;
+    }
+};
