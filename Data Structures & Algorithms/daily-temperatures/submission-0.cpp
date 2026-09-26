3df4e6704768efaeefaa3dffee1c55011036734f
sainidia333-png <saini.dia333@gmail.com>
2026-09-27 00:33:41 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int>v(temperatures.size(), 0);
+        stack<pair<int,int>>s;
+        int index=0;
+        s.push({0,temperatures[0]});
+        for(int i=1;i<temperatures.size();i++){
+            while(!s.empty()&&temperatures[i]>s.top().second){
+                //s.pop();
+               v[s.top().first] = i - s.top().first;
+                s.pop();
+                
+            }
+           s.push({i,temperatures[i]});
+        }
+        return v;
+    }
+};
